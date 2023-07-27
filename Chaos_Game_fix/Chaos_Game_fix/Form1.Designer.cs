a9ab282d1c959cb7cde88325f443e9e76f0157af
namlessuM6 <115329816+namlessuM6@users.noreply.github.com>
2023-07-26 20:50:18 -0700

    Add files via upload

---
+++ Chaos_Game_fix/Chaos_Game_fix/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Chaos_Game_fix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redbtn = new System.Windows.Forms.Button();
+            this.bluebtn = new System.Windows.Forms.Button();
+            this.greenbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // redbtn
+            // 
+            this.redbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.redbtn.Location = new System.Drawing.Point(241, 44);
+            this.redbtn.Name = "redbtn";
+            this.redbtn.Size = new System.Drawing.Size(75, 23);
+            this.redbtn.TabIndex = 0;
+            this.redbtn.Text = "Red";
+            this.redbtn.UseVisualStyleBackColor = true;
+            this.redbtn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // bluebtn
+            // 
+            this.bluebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.bluebtn.Location = new System.Drawing.Point(10, 400);
+            this.bluebtn.Name = "bluebtn";
+            this.bluebtn.Size = new System.Drawing.Size(75, 23);
+            this.bluebtn.TabIndex = 1;
+            this.bluebtn.Text = "Blue";
+            this.bluebtn.UseVisualStyleBackColor = true;
+            this.bluebtn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // greenbtn
+            // 
+            this.greenbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.greenbtn.Location = new System.Drawing.Point(470, 400);
+            this.greenbtn.Name = "greenbtn";
+            this.greenbtn.Size = new System.Drawing.Size(75, 23);
+            this.greenbtn.TabIndex = 2;
+            this.greenbtn.Text = "Green";
+            this.greenbtn.UseVisualStyleBackColor = true;
+            this.greenbtn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.greenbtn);
+            this.Controls.Add(this.bluebtn);
+            this.Controls.Add(this.redbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button redbtn;
+        private System.Windows.Forms.Button bluebtn;
+        private System.Windows.Forms.Button greenbtn;
+    }
+}
+
---
+++ Chaos_Game_fix/Chaos_Game_fix/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Chaos_Game_fix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private float x = 470;
+        private float y = 400;
+        Graphics g;
+        Timer timer= new Timer();
+        SolidBrush brush = new SolidBrush(Color.Black);
+        Pen pen = new Pen(Color.Black);
+        PointF[] outerpoints = new PointF[] {
+                new PointF(85F, 400F),
+                new PointF(470F, 400F),
+                new PointF(277.5F, 66.580F)
+            };
+
+        PointF[] innerpoints = new PointF[] {
+                new PointF(277.5F, 400F),
+                new PointF(181.25F, 231.044F),
+                new PointF(369.75F, 231.044F)
+            };
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            x = xdistance(btn.Location.X);
+            g.Clear(Color.White);
+            y = ydistance(btn.Location.Y);
+            timer.Start();
+
+        }
+
+
+        private float xdistance(float btnx)
+        {
+            float midpoint = (x - btnx) / 2;
+            x = x - midpoint;
+            //MessageBox.Show("new x coordinate: " + x);
+            return x;
+        }
+
+        private float ydistance(float btny)
+        {
+            float midpoint = (y - btny) / 2;
+            y = y - midpoint;
+            //MessageBox.Show("new y coordinate: " + y);
+            return y;
+        }
+
+
+        private float midx (float x1, float x2)
+        {
+            return 0;
+        }
+
+        private float midy(float y1, float y2)
+        {
+            return 0;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            g = this.CreateGraphics();
+            timer.Interval = 1;
+            timer.Tick += Timer_Tick;
+            int level = Levelselection();
+            while (level == 0) {
+                MessageBox.Show("level must be greater then 0");
+                level = Levelselection();
+            } ;
+            timer.Start();
+        }
+
+        private int Levelselection()
+        {
+            string level = Interaction.InputBox("What Level Would you Like to play? level must be greater then 1", "level selection", "1");
+            try
+            {
+                int levelnum = Int32.Parse(level);
+                return levelnum;
+
+            }
+            catch 
+            {
+                
+            }
+            return 0;
+        }
+        /*private PointF Innertri()
+        {
+
+            return null;
+        }*/
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            Rectangle r = new Rectangle((int)x, (int)y, 50, 50);
+            g.DrawPolygon(pen, outerpoints);
+            g.DrawPolygon(pen, innerpoints);
+            g.FillEllipse(brush, r);
+            timer.Stop();
+
+        }
+
+
+    }
+}
