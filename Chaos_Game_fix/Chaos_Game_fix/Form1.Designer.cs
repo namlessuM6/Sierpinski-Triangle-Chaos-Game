f062ab4a6ba8f9c4f8b4a9c17c471ab843a7dd12
namlessuM6 <115329816+namlessuM6@users.noreply.github.com>
2023-08-06 18:36:43 -0700

    Add files via upload

--- Chaos_Game_fix/Chaos_Game_fix/Form1.Designer.cs
+++ Chaos_Game_fix/Chaos_Game_fix/Form1.Designer.cs
@@ -34,18 +34,18 @@
             this.greenbtn = new System.Windows.Forms.Button();
             this.startbtn = new System.Windows.Forms.Button();
             this.controler = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.resizebtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.controler)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // redbtn
             // 
             this.redbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.redbtn.AutoSize = true;
-            this.redbtn.Location = new System.Drawing.Point(277, 65);
+            this.redbtn.Location = new System.Drawing.Point(337, 68);
+            this.redbtn.MaximumSize = new System.Drawing.Size(100, 30);
+            this.redbtn.MinimumSize = new System.Drawing.Size(75, 30);
             this.redbtn.Name = "redbtn";
-            this.redbtn.Size = new System.Drawing.Size(75, 23);
+            this.redbtn.Size = new System.Drawing.Size(75, 30);
             this.redbtn.TabIndex = 0;
             this.redbtn.Text = "Red";
             this.redbtn.UseVisualStyleBackColor = true;
@@ -54,10 +54,11 @@
             // bluebtn
             // 
             this.bluebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.bluebtn.AutoSize = true;
-            this.bluebtn.Location = new System.Drawing.Point(84, 397);
+            this.bluebtn.Location = new System.Drawing.Point(144, 400);
+            this.bluebtn.MaximumSize = new System.Drawing.Size(100, 30);
+            this.bluebtn.MinimumSize = new System.Drawing.Size(75, 30);
             this.bluebtn.Name = "bluebtn";
-            this.bluebtn.Size = new System.Drawing.Size(75, 23);
+            this.bluebtn.Size = new System.Drawing.Size(75, 30);
             this.bluebtn.TabIndex = 1;
             this.bluebtn.Text = "Blue";
             this.bluebtn.UseVisualStyleBackColor = true;
@@ -66,10 +67,11 @@
             // greenbtn
             // 
             this.greenbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.greenbtn.AutoSize = true;
-            this.greenbtn.Location = new System.Drawing.Point(469, 397);
+            this.greenbtn.Location = new System.Drawing.Point(529, 400);
+            this.greenbtn.MaximumSize = new System.Drawing.Size(100, 30);
+            this.greenbtn.MinimumSize = new System.Drawing.Size(75, 30);
             this.greenbtn.Name = "greenbtn";
-            this.greenbtn.Size = new System.Drawing.Size(75, 23);
+            this.greenbtn.Size = new System.Drawing.Size(75, 30);
             this.greenbtn.TabIndex = 2;
             this.greenbtn.Text = "Green";
             this.greenbtn.UseVisualStyleBackColor = true;
@@ -77,10 +79,12 @@
             // 
             // startbtn
             // 
-            this.startbtn.AutoSize = true;
-            this.startbtn.Location = new System.Drawing.Point(682, 56);
+            this.startbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.startbtn.Location = new System.Drawing.Point(662, 68);
+            this.startbtn.MaximumSize = new System.Drawing.Size(100, 30);
+            this.startbtn.MinimumSize = new System.Drawing.Size(75, 30);
             this.startbtn.Name = "startbtn";
-            this.startbtn.Size = new System.Drawing.Size(75, 23);
+            this.startbtn.Size = new System.Drawing.Size(75, 30);
             this.startbtn.TabIndex = 3;
             this.startbtn.Text = "Start";
             this.startbtn.UseVisualStyleBackColor = true;
@@ -89,36 +93,38 @@
             // controler
             // 
             this.controler.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("controler.BackgroundImage")));
-            this.controler.Location = new System.Drawing.Point(459, 397);
+            this.controler.Location = new System.Drawing.Point(529, 400);
             this.controler.Name = "controler";
-            this.controler.Size = new System.Drawing.Size(4, 4);
+            this.controler.Size = new System.Drawing.Size(5, 5);
             this.controler.TabIndex = 6;
             this.controler.TabStop = false;
             // 
-            // panel1
+            // resizebtn
             // 
-            this.panel1.Controls.Add(this.startbtn);
-            this.panel1.Controls.Add(this.greenbtn);
-            this.panel1.Controls.Add(this.bluebtn);
-            this.panel1.Controls.Add(this.redbtn);
-            this.panel1.Controls.Add(this.controler);
-            this.panel1.Location = new System.Drawing.Point(1, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(799, 449);
-            this.panel1.TabIndex = 7;
+            this.resizebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.resizebtn.Location = new System.Drawing.Point(662, 145);
+            this.resizebtn.MaximumSize = new System.Drawing.Size(75, 35);
+            this.resizebtn.MinimumSize = new System.Drawing.Size(75, 35);
+            this.resizebtn.Name = "resizebtn";
+            this.resizebtn.Size = new System.Drawing.Size(75, 35);
+            this.resizebtn.TabIndex = 7;
+            this.resizebtn.Text = "Resize triangle";
+            this.resizebtn.UseVisualStyleBackColor = true;
+            this.resizebtn.Click += new System.EventHandler(this.resizebtn_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(919, 573);
+            this.Controls.Add(this.resizebtn);
+            this.Controls.Add(this.startbtn);
+            this.Controls.Add(this.greenbtn);
+            this.Controls.Add(this.bluebtn);
+            this.Controls.Add(this.redbtn);
+            this.Controls.Add(this.controler);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.controler)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -130,7 +136,7 @@
         private System.Windows.Forms.Button greenbtn;
         private System.Windows.Forms.Button startbtn;
         private System.Windows.Forms.PictureBox controler;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button resizebtn;
     }
 }
 
--- Chaos_Game_fix/Chaos_Game_fix/Form1.cs
+++ Chaos_Game_fix/Chaos_Game_fix/Form1.cs
@@ -17,96 +17,98 @@ namespace Chaos_Game_fix
         {
             InitializeComponent();
         }
-        private float x = 470;
-        private float y = 400;
+        //calss variables
+        //x coordinate of controller
+        private float x;
+        //y coordinate of controller
+        private float y;
+        //level selection variable
         int level;
+        //Graphics object to render drawings
         Graphics g;
-        Timer timer= new Timer();
-        SolidBrush brush = new SolidBrush(Color.Black);
+        //pen Object for drawing
         Pen pen = new Pen(Color.Black);
-        PointF[] outerpoints = new PointF[] {
-                new PointF(85F, 400F),
-                new PointF(470F, 400F),
-                new PointF(277.5F, 66.580F)
-            };
 
-        PointF[] innerpoints = new PointF[] {
-                new PointF(277.5F, 400F),
-                new PointF(181.25F, 231.044F),
-                new PointF(369.75F, 231.044F)
-            };
+
+        /// <summary>
+        /// method for corner button button actions
+        /// </summary>
+        /// <param name="sender"> tells the method which button was clicked</param>
+        /// <param name="e"> allows method to be called on button click</param>
         private void btn_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
-            x = xdistance(btn.Location.X);
-            y = ydistance(btn.Location.Y);
-            timer.Start();
+            //calls mid method to find new points for controller
+            x = mid(x, btn.Location.X);
+            y = mid(y, btn.Location.Y);
+            //applies new points to controller
+            controler.Location = new Point((int)x, (int)y);
 
         }
 
 
-        private float xdistance(float btnx)
+        /// <summary>
+        /// method to calculate the midpoint of two coordinate values
+        /// method needs to be called twice for midpoints of points
+        /// once for x-coordinate once for y-coordinate
+        /// </summary>
+        /// <param name="y1"></param>
+        /// <param name="y2"></param>
+        /// <returns>calculated coordinate value</returns>
+        private float mid(float y1, float y2)
         {
-            float midpoint = (x - btnx) / 2;
-            x = x - midpoint;
-            return x;
-        }
-
-        private float ydistance(float btny)
-        {
-            float midpoint = (y - btny) / 2;
-            y = y - midpoint;
-            return y;
-        }
-
-
-        private float midx (float x1, float x2)
-        {
-            
-            return Math.Abs((x1 + x2)/2) ;
-        }
-
-        private float midy(float y1, float y2)
-        {
+            //absolute value of y1+y2 divided by 2
             return Math.Abs((y1+y2)/2);
         }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            g = this.CreateGraphics();
-            timer.Interval = 1;
-            timer.Tick += Timer_Tick;
-            timer.Start();
-        }
 
+
+        /// <summary>
+        /// allows user to select level to play
+        /// </summary>
+        /// <returns> selected level or 0 upon error</returns>
         private int Levelselection()
         {
+            //message box asking user to input level default input is 1
             string level = Interaction.InputBox("What Level Would you Like to play? level must be greater then 1", "level selection", "1");
             try
             {
+                //convert input string to int value
                 int levelnum = Int32.Parse(level);
                 return levelnum;
-
             }
             catch 
             {
-                
+                //upon error of conversion above no converson is done and 0 is returned
             }
             return 0;
         }
+
+        /// <summary>
+        /// method used to draw and calculate inner triangles
+        /// </summary>
+        /// <param name="n">counter that keps track of </param>
+        /// <param name="x1"></param>
+        /// <param name="y1"></param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <param name="x3"></param>
+        /// <param name="y3"></param>
+        /// <returns></returns>
         private PointF[] Innertri(int n, float x1, float y1, float x2, float y2, float x3, float y3)
         {
             int l = level;
-            float a = midx(x1, x2);
-            float b = midy(y1, y2);
-            float c = midy(x2, x3);
-            float d = midy(y2, y3);
-            float e = midy(x1, x3);
-            float f = midy(y1, y3); 
+            double ammount = Math.Pow(3, level);
+            float a = mid(x1, x2);
+            float b = mid(y1, y2);
+            float c = mid(x2, x3);
+            float d = mid(y2, y3);
+            float e = mid(x1, x3);
+            float f = mid(y1, y3); 
             PointF[] tripoints = new PointF[3];
             tripoints[0] = new PointF(a, b);
             tripoints[1] = new PointF(c, d);
             tripoints[2] = new PointF(e, f);
-            if (l > n)
+            if(l > n)
             {
                 n++;
                 g.DrawPolygon(pen, Innertri(n, x1, y1, a, b, e, f));
@@ -117,33 +119,73 @@ namespace Chaos_Game_fix
             return tripoints;
         }
 
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            controler.Location = new System.Drawing.Point((int)x, (int)y);
-            timer.Stop();
-
-        }
-
         private void startbtn_Click(object sender, EventArgs e)
         {
+            Refreshform();
+            startbtn.Location = new Point(redbtn.Location.X + 300, redbtn.Location.Y);
+            resizebtn.Location = new Point(startbtn.Location.X, startbtn.Location.Y + 80);
             g.Clear(Color.White);
+            PointF[] outerpoints = new PointF[] {
+                new PointF(bluebtn.Location.X, bluebtn.Location.Y),
+                new PointF(greenbtn.Location.X, greenbtn.Location.Y),
+                new PointF(redbtn.Location.X, redbtn.Location.Y)
+            };
             g.DrawPolygon(pen, outerpoints);
-
             level = Levelselection();
-            x = 470;
-            y = 400;
-            controler.Location = new System.Drawing.Point((int)x, (int)y);
+            x = greenbtn.Location.X;
+            y = greenbtn.Location.Y;
+            controler.Location = new Point((int)x, (int)y);
             while (level == 0)
             {
                 MessageBox.Show("level must be greater then 0");
                 level = Levelselection();
             };
             int n = 1;
-
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             g.DrawPolygon(pen, Innertri(n, bluebtn.Location.X, bluebtn.Location.Y, redbtn.Location.X, redbtn.Location.Y, greenbtn.Location.X, greenbtn.Location.Y));
-
+            watch.Stop();
+            var mils = watch.ElapsedMilliseconds;
+            MessageBox.Show("That took " + (float)mils/1000 + " seconds");
         }
 
+        private Point btnloc (float scale, Button btn)
+        {
+            
+            int x = (int)btn.Location.X * (int)scale;
+            int y = (int)btn.Location.Y * (int)scale;
+            Point cor = new Point(x, y);
+            return cor;
 
+            
+        }
+        private void Refreshform ()
+        {
+            this.Refresh();
+            g = this.CreateGraphics();
+        }
+
+        private void resizebtn_Click(object sender, EventArgs e)
+        {
+            float scale;
+            string scalestr = Interaction.InputBox("how much larger do you want the triangle to be? scaler must be greater then 0", "Resize", "1");
+            try
+            {
+                scale = float.Parse(scalestr);
+                
+
+            }
+            catch
+            {
+                MessageBox.Show("Failure you must enter a valid number");
+                scale = 1;
+            }
+            redbtn.Location = new Point((Size)btnloc(scale, redbtn));
+            greenbtn.Location = new Point((Size)btnloc(scale, greenbtn));
+            bluebtn.Location = new Point((Size)btnloc(scale, bluebtn));
+            redbtn.Location = new Point(redbtn.Location.X - (int)(300 * scale), redbtn.Location.Y);
+            greenbtn.Location = new Point(greenbtn.Location.X - (int)(300 * scale), greenbtn.Location.Y);
+            bluebtn.Location = new Point(bluebtn.Location.X - (int)(300* scale), bluebtn.Location.Y);
+            
+        }
     }
 }
